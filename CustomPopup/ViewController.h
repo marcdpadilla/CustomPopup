e72ecd8ab107cd2039a4df544911c6d7bf561a9b
Marc Padilla <marcpadilla@EGG686-YONDU-MP.local>
2014-08-28 15:03:17 +0800

    Initial Commit

---
+++ CustomPopup/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CustomPopup
+//
+//  Created by Marc Padilla on 8/28/14.
+//  Copyright (c) 2014 Yondu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CustomPopupTests/CustomPopupTests.m
@@ -0,0 +1,34 @@
+//
+//  CustomPopupTests.m
+//  CustomPopupTests
+//
+//  Created by Marc Padilla on 8/28/14.
+//  Copyright (c) 2014 Yondu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CustomPopupTests : XCTestCase
+
+@end
+
+@implementation CustomPopupTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
